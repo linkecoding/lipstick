fe715edc5f9066368933ac145a9dd3e13fb4f53a
linkecoding <szher@qq.com>
2022-03-26 17:52:36 +0800

    feat: 替换js库链接

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const minify = require("gulp-minify");
 const jsonminify = require("gulp-jsonminify");
 const del = require("del");
-
+const destDir = "dist/lipstick"
 gulp.task("clean", function (cb) {
   (async () => {
     const deletedPaths = await del("dist/");
@@ -18,14 +18,13 @@ gulp.task("minify", function (cb) {
         noSource: true,
       })
     )
-    .pipe(gulp.dest("dist"));
-  gulp.src(["src/lipstick.json"]).pipe(jsonminify()).pipe(gulp.dest("dist"));
+    .pipe(gulp.dest(destDir));
+  gulp.src(["src/lipstick.json"]).pipe(jsonminify()).pipe(gulp.dest(destDir));
   cb();
 });
 
 gulp.task("copy-static", function (cb) {
-  gulp.src("lib/*.js").pipe(gulp.dest("dist/lib/"));
-  gulp.src("index.html").pipe(gulp.dest("dist/"));
+  gulp.src("index.html").pipe(gulp.dest(destDir));
   cb();
 });
 
